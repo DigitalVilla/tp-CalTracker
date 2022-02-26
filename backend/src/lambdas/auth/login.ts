2740ff31535b26a630c4dc264b907103f07c7e5f
DigitalVilla <digitalvilla01@gmail.com>
2022-02-26 13:18:13 -0600

    feat: create lambda endpoints for auth

--- backend/src/lambdas/auth/login.ts
+++ backend/src/lambdas/auth/login.ts
@@ -1,9 +1,58 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { sanitizeUser } from '../../utils/sanitizeUser'
+import { getUser } from '../../db/getUser'
+import { createToken, hashValue } from '../../utils/hash'
 import { response } from '../../utils/response'
+import { isValid } from '../../utils/validations'
 
+/**
+ *
+ * @method POST
+ * @queryParams clean - (optional) will return only the token
+ * @body - { email, password}
+ * @returns - token and user is specified in url
+ */
 export async function handler(
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
-  console.log(event)
-  return response.success('success', 200)
+  try {
+    console.log(event)
+
+    const { email, password } = JSON.parse(event.body || '{}')
+
+    if (!email) return response.error('Missing Email', 400)
+    if (!password) return response.error('Missing Password', 400)
+
+    isValid.email({ value: email })
+    isValid.password({ value: password })
+
+    // Retrieve Item
+    const Item = await getUser(email)
+
+    // Validate Passwords
+    if (!Item || Item.Password !== hashValue(password)) {
+      console.log(`Invalid password`)
+      return response.error('Invalid username or password', 404)
+    }
+
+    // Create Token
+    const payload: Record<string, any> = {}
+    payload.token = createToken(
+      {
+        email: Item.Email,
+        role: Item.Role,
+        id: Item.Id,
+      },
+      '7d'
+    )
+
+    // Sanitize Item as User
+    if (typeof event.queryStringParameters?.clean !== 'string') {
+      payload.user = sanitizeUser(Item)
+    }
+
+    return response.success(payload, 201)
+  } catch (error: any) {
+    return response.error(error)
+  }
 }
--- backend/src/lambdas/auth/signup.ts
+++ backend/src/lambdas/auth/signup.ts
@@ -1,7 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import { setUser } from '../../actions/setUser'
+import { setUser } from '../../db/setUser'
 import { response } from '../../utils/response'
-import { isInvalid } from '../../utils/validations'
+import { isValid } from '../../utils/validations'
 
 export async function handler(
   event: APIGatewayProxyEvent
@@ -9,19 +9,30 @@ export async function handler(
   try {
     console.log(event)
 
-    const { email, password } = JSON.parse(event.body || '{}')
+    const { email, password, username, age, budget, maxCals } = JSON.parse(
+      event.body || '{}'
+    )
     if (!email) return response.error('Missing Email', 400)
     if (!password) return response.error('Missing Password', 400)
 
-    const isEmailInvalid = isInvalid.email(email)
-    const isPassInvalid = isInvalid.password(password)
+    isValid.email({ value: email })
+    isValid.password({ value: password })
 
-    if (isEmailInvalid) return response.error(isEmailInvalid, 400)
-    if (isPassInvalid) return response.error(isPassInvalid, 400)
+    const set = await setUser({
+      email,
+      password,
+      username,
+      budget,
+      maxCals,
+      age,
+    })
 
-    const message = await setUser({ email, password })
-    return response.success(message, 201)
+    const message = set
+      ? `User ${email} was created`
+      : `User ${email} already exist`
+
+    return response.success(message, set ? 201 : 400)
   } catch (error: any) {
-    return response.error(error.message ? error.message : error, error?.code)
+    return response.error(error)
   }
 }
